3c3fa577cefa36dbfbe4415b19601e698bfbf968
Zach Milne <zachmilne@Zachs-MacBook-Pro.local>
2021-04-09 23:20:28 -0400

    added secret

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors') ;//foo
 var express = require('express');
+require('dotenv').config();
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/test', (req, res, next) => {
+  res.send(process.env.API_SECRET);
+})
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
