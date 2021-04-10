259dbb5198cfc47d5fd943039082178bf5641056
Zach Milne <zachmilne@Zachs-MacBook-Pro.local>
2021-04-09 23:51:25 -0400

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 const client = require('superagent');
-const { response } = require('../app');
 
 router.use(async (resp, req, next) => {
   try {
